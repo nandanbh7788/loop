c735318e3965386562d70da712d0587ea17b756f
nandan <nandanbh7788@gmail.com>
2023-03-01 16:37:50 +0530

    version 1

---
+++ Loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            
+            for (int i = 1; i <= rows; ++i)
+            {
+
+                
+                for (int j = 1; j <= i; ++j)
+                {
+                    Console.WriteLine(j + " ");
+                }
+                Console.WriteLine("");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+   
